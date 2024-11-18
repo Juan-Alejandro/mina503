493e3365d2669d23b2d04e6b23e617fbc4a76e3a
Juan-Alejandro-Arriaga <juanalejandroarriaganava@gmail.com>
2024-11-18 17:14:54 -0600

    v.13

--- javascript/index.js
+++ javascript/index.js
@@ -22,3 +22,33 @@
             function abriris() {
                 window.open("html/inicio_secion.html","_self")
             }
+
+/*MENU DE PUNTO DE VENTA*/
+            document.addEventListener("DOMContentLoaded", function () {
+                const icoham = document.querySelector(".ico_ham_pventa");
+                const lisnav = document.querySelector(".div_perfil_pventa");
+                icoham.addEventListener("click", function () {
+                    lisnav.classList.toggle("active");
+                });
+            });
+
+        const archivo = document.getElementById('input_cargarimagen_pventa');
+        const imgg = document.getElementById('img_perfil_pventa');
+        archivo.addEventListener('change', function () {
+            const obar = this.files[0]; 
+            if (obar) {
+                const lector = new FileReader();
+                lector.onload = function (event) {
+                    const dimg = event.target.result;
+                    imgg.src = dimg;
+                    localStorage.setItem('ImagenAlm', dimg);
+                };
+                lector.readAsDataURL(obar); 
+            }
+        });
+        window.addEventListener('load', function () {
+            const Imgalm = localStorage.getItem('ImagenAlm');
+            if (Imgalm) {
+                imgg.src = Imgalm; // Mostrar la imagen almacenada
+            }
+        });
