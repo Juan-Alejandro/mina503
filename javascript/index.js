496aaf5b20e5e1b7c952fbd9d63f90712c76a466
Juan-Alejandro-Arriaga <juanalejandroarriaganava@gmail.com>
2024-11-18 23:06:46 -0600

    v.15

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 /*CABECERA*/
+            /*MENU DESPLEGABLE MOVIL*/
             document.addEventListener("DOMContentLoaded", function () {
                 const icoham = document.querySelector(".ico_ham");
                 const lisnav = document.querySelector(".lis_nav_cabecera");
@@ -9,13 +10,14 @@
 
 
 /*CUERPO*/
+            /*PREGUNTA DE REDIRECCIONADOR*/
             function pregunta_redireccionador(){
                 const confirmacion = confirm("¿Estás seguro de que deseas abandonar el sitio?");
                 if(!confirmacion) {
                     event.preventDefault();
                 }
             }
-
+            /*ABRIR COSAS*/
             function abrirstandby() {
                 window.open("html/standby.html","_self");
             }
@@ -24,6 +26,7 @@
             }
 
 /*MENU DE PUNTO DE VENTA*/
+            /*MENU DESPLEGABLE MOVIL*/
             document.addEventListener("DOMContentLoaded", function () {
                 const icoham = document.querySelector(".ico_ham_pventa");
                 const lisnav = document.querySelector(".div_perfil_pventa");
@@ -31,24 +34,28 @@
                     lisnav.classList.toggle("active");
                 });
             });
-
-        const archivo = document.getElementById('input_cargarimagen_pventa');
-        const imgg = document.getElementById('img_perfil_pventa');
-        archivo.addEventListener('change', function () {
-            const obar = this.files[0]; 
-            if (obar) {
-                const lector = new FileReader();
-                lector.onload = function (event) {
-                    const dimg = event.target.result;
-                    imgg.src = dimg;
-                    localStorage.setItem('ImagenAlm', dimg);
-                };
-                lector.readAsDataURL(obar); 
-            }
-        });
-        window.addEventListener('load', function () {
-            const Imgalm = localStorage.getItem('ImagenAlm');
-            if (Imgalm) {
-                imgg.src = Imgalm; // Mostrar la imagen almacenada
-            }
-        });
+            /*CARGADOR DE IMAGEN*/
+                const upicon = document.getElementById('icon_photo_pventa');
+                const archivo = document.getElementById('input_cargarimagen_pventa');
+                upicon.addEventListener('click', () =>{
+                    archivo.click();
+                })
+                const imgg = document.getElementById('img_perfil_pventa');
+                archivo.addEventListener('change', function () {
+                    const obar = this.files[0]; 
+                    if (obar) {
+                        const lector = new FileReader();
+                        lector.onload = function (event) {
+                            const dimg = event.target.result;
+                            imgg.src = dimg;
+                            localStorage.setItem('ImagenAlm', dimg);
+                        };
+                        lector.readAsDataURL(obar); 
+                    }
+                });
+                window.addEventListener('load', function () {
+                    const Imgalm = localStorage.getItem('ImagenAlm');
+                    if (Imgalm) {
+                        imgg.src = Imgalm;
+                    }
+                });
